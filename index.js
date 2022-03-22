85f058c75ae48634d9915ac4a22f8a37cae9bbda
Kanna <mail@kannadev.com>
2022-03-22 19:00:09 +0100

    Initial Commit :3

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: 32767,
+});
+module.exports = client;
+const dotenv = require('dotenv');
+dotenv.config();
+// Global Variables
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+// Initializing the project
+require("./handler")(client);
+
+client.login(process.env.DISCORD_TOKEN);
