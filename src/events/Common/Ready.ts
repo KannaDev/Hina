f172ae88c83fb625668e9d3ff8890d37d3d9e2b4
Kanna <mail@kannadev.com>
2022-04-03 04:14:46 +0200

    changes before i go to sleep and new script

--- src/events/Common/Ready.ts
+++ src/events/Common/Ready.ts
@@ -5,9 +5,20 @@ export default new Event("ready", interaction => {
   console.log(`${client.user.username} is now online`)
   console.log(`Guilds: ${client.guilds.cache.size}`)
   console.log(`Users: ${client.users.cache.size}`)
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const revision = require("child_process")
+    .execSync("git rev-parse HEAD")
+    .toString()
+    .trim()
+    .toString()
+    .trim()
+  const information = new MessageEmbed()
+    .setTitle("Client Info")
+    .setDescription(
+      `Guilds: ${client.guilds.cache.size}\nUsers: ${client.users.cache.size}\nCurrent Version: [${revision}](https://github.com/KannaDev/Hina/commit/${revision})`
+    )
 
-  const boop = new MessageEmbed().setTitle("heya")
   client.users.fetch("317728561106518019").then(owner => {
-    owner.send({ embeds: [boop] })
+    owner.send({ embeds: [information] })
   })
 })
